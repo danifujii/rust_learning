0c4a2be6053425d6567050af06f277968eed22fd
Daniel Fujii <danifujii@gmail.com>
2020-05-08 09:03:47 -0300

    Return JSON as resp and delete custom error type

--- witter-backend/src/main.rs
+++ witter-backend/src/main.rs
@@ -1,43 +1,30 @@
 use dotenv;
-
-use tide::{Server, Request};
-
+use tide::{Server, StatusCode, Request, Response};
+use serde_json::json;
 use sqlx::{PgPool, Pool, query};
 
 #[async_std::main]
-async fn main() -> Result<(), Error> {
+async fn main() {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let db_url = std::env::var("DATABASE_URL")?;
-    let db_pool: PgPool = Pool::new(&db_url).await?;
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+    let db_pool: PgPool = Pool::new(&db_url).await.unwrap();
 
     let mut app: Server<State> = Server::with_state(State{ db_pool });
 
     app.at("/").get(|req: Request<State>| async move {
-        let db_pool = &req.state().db_pool;
-        let rows = query!("select 1 as one").fetch_one(db_pool).await?;
+        /* let db_pool = &req.state().db_pool;
+        let rows = query!("select 1 as one where 1 = 2").fetch_one(db_pool).await.unwrap();
         dbg!(rows);
-        Ok("Hello, world!")
+        Ok("Hello, world!") */
+        let json = json!([1, 2, 3]);
+        Ok(Response::new(StatusCode::Ok).body_json(&json)?)
     });
-    app.listen("127.0.0.1:8080").await?;
-
-    Ok(())
+    app.listen("127.0.0.1:8080").await.unwrap();
 }
 
 #[derive(Debug)]
 struct State {
     db_pool: PgPool,
-}
-
-#[derive(thiserror::Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    DbError(#[from] sqlx::Error),
-
-    #[error(transparent)]
-    IoError(#[from] std::io::Error),
-
-    #[error(transparent)]
-    VarError(#[from] std::env::VarError),
 }
